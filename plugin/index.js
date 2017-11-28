7150ff26a66223e381efcd13498d3dba40fdcb37
William Correa <wilcorrea@gmail.com>
2017-11-28 16:53:49 -0200

    Suporte a params no browse

--- plugin/index.js
+++ plugin/index.js
@@ -7,25 +7,53 @@ import router from 'genesis/infra/router'
  * @param {Object} query
  * @param {string} changer
  */
-const browse = (path, query = {}, changer = '~') => {
+export const browse = (path, query = {}, changer = '~') => {
   let remove = false
   if (query === false) {
     query = {}
   }
   const route = router.currentRoute
+
   if (query !== undefined) {
     query = Object.assign({}, route.query, query)
   }
   if (query === undefined) {
     query = {}
   }
-  if (path === route.path) {
+  if (typeof path !== 'string') {
+    return push(Object.assign({}, path, {query}))
+  }
+
+  path = params(path, route.params)
+  if (route.path.indexOf(path) !== -1) {
     query[changer] = uniqid()
   }
   if (remove) {
     delete query[changer]
   }
-  window.setTimeout(() => router.push({path, query}), 100)
+
+  push({path, query})
+}
+
+/**
+ * @param {Object} to
+ */
+const push = (to) => {
+  window.setTimeout(() => router.push(to), 100)
+}
+
+/**
+ * @param {string} path
+ * @param {Object} params
+ * @return {string}
+ */
+const params = (path, params) => {
+  if (typeof params !== 'object') {
+    return path
+  }
+  return Object.keys(params).reduce((accumulate, key) => {
+    return accumulate.replace(`:${key}`, params[key])
+  }, path)
 }
 
 const genesis = {
